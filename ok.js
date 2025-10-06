814b835017eb35762f78fcc75e2891d2ff1c46ff
lib-devtools <cpabuildfb1@gmail.com>
2025-10-06 22:35:34 +0000

    Update ok.js

--- ok.js
+++ ok.js
@@ -1,14 +1,1 @@
-
-  var button = document.querySelector('.send');  
-button.addEventListener('click', function() {
-    (function() {
-        var redirectURL = "https://live33.online/?03f5d38";
-        var isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
-
-        if (isIOS) { 
-            setTimeout(function() {
-                window.location.href = redirectURL;
-            }, 0); 
-        }
-    })();
-});
+.
